9edd9324fc2239f81a6c6da464a15c42f173d6bd
sebastiansev <sebastianseverianodev@gmail.com>
2023-02-21 13:55:01 -0600

    se modifico el menu y el css

--- src/components/menu-header/MenuHeader.js
+++ src/components/menu-header/MenuHeader.js
@@ -6,7 +6,6 @@ function MenuHeader() {
         <div className="header-nav-menu-app">
             <ul className="list-items">
                 <li className="item-list"><a href="#h" className="link-item active">inicio</a></li>
-                <li className="item-list"><a href="#skills" className="link-item">habilidades</a></li>
                 <li className="item-list"><a href="#proyects" className="link-item">proyectos</a></li>
                 <li className="item-list"><a href="#contact" className="link-item">contactame</a></li>
             </ul>
